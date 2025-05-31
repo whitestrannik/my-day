a5685827b073b9c4e1609cac310ccac641d1a302
Michael <bazhanovm@outlook.com>
2025-05-30 21:12:21 -0700

    Preparation for deployment to GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-day/',
   plugins: [react(), tailwindcss()],
   test: {
     globals: true,
